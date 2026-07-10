8a13f18628189c20d1ce09fc34035d4586baef30
jamesmaccoy <thankyou.digital@gmail.com>
2026-07-10 17:07:25 +0200

    Update workData.js

--- src/components/work/workData.js
+++ src/components/work/workData.js
@@ -116,6 +116,11 @@ export const allProjects = [
     quote: "Validating the MVP: Ensure there is sufficient customer demand for the product, before investing in scalability, and navigating through the prejudice in our country’s long history of prohibition",
     outcome: "Widely shared on social platforms, and appearing on influential blogs. The interest in the offering seemed to have peaked, which led to customers going through email funnels and of course a few signups.",
     approach: "An executed polished product ready for high daily traffic. All user journeys, and touch points considered, with payment options and mobile notifications of each phase of the cycle.",
+    challenges: "Building a new venture & the role of digital in the exploding cannabis industry.",
+    design: "Building a new venture & the role of digital in the exploding cannabis industry.",
+    development: "Building a new venture & the role of digital in the exploding cannabis industry.",
+
+
   },
 
   {
